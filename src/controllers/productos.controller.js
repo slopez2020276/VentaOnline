f681d0ec69998ca8dde3efd069079d18e6678514
slopez2020276 <slopez-2020276@kinal.edu.gt>
2022-03-11 06:13:02 -0600

    productos crud completo

--- src/controllers/productos.controller.js
+++ src/controllers/productos.controller.js
@@ -62,7 +62,73 @@ function AgregarProducto (req, res){
 }
 
 
+function EditarProducto(req, res){
+
+    var categoriaId = req.params.idCategoria;
+    var productosId = req.params.idPructos;
+    var parametros = req.body;
+    
+    if(parametros.stock){
+        Productos.findById(productosId,(err,productosEncontrados)=>{
+           if(err) return res.status(500).send({message:'error en la peticion del producto'})
+           if(productosEncontrados){
+             Categoria.findOne({_id:categoriaId,productosId: productosId},(err,categoriaEncontrada)=>{
+                 if(err) return res.status(500).send({message:'error en la peticion de categoria'});
+                 if(categoriaEncontrada){
+                     Productos.findByIdAndUpdate(productosId,parametros,{new: true},(err,productoActualizado)=>{
+                        if(err) return res.status(500).send({message:''})
+                        if(!productoActualizado){
+                            return res.status(500).send({message:'el producto no se acualizo'});
+                        }else{
+                            return res.status(200).send({producto:productoActualizado})
+                        }
+                     })
+                 }
+
+             })
+           }else{
+               return res.status(500).send({message:'el producto no existe pa'})
+
+
+           }
+           
+        })
+
+
+    }else{
+        return res.status(500).send({message:'debe de llenar los parametos obligatorios'});
+    }
+}
+function eliminarProducto(req, res){
+    var categoriaId = req.params.idCategoria;
+    var productosId = req.params.idPructos;
+
+    Categoria.findOneAndUpdate({_id:categoriaId,productos: productosId},{$pull:{productos:productosId}},{new: true},(err,categoriaActializada)=>{
+
+     if(err) return res.status(500).send({message:'error en la peticion '})
+       if(!categoriaActializada){
+           Productos.findByIdAndRemove(productosId,(err,productoElinado)=>{
+              if(err) return res.status(500).send({message:'erro al eliminar el producto'});
+              if(!productoElinado){
+                   return res.status(500).send({message:'no se elimino el producto'})
+              }else{
+                  return res.status(200).send({productos:productoElinado})
+              }
+
+           })
+
+       }else{
+           return res.status(500).send({message:'el producto no existe'})
+       }
+    })
+}
+
+
+
+
 module.exports = {
 
     AgregarProducto,
+    EditarProducto,
+    eliminarProducto
 }
--- src/routes/productos.routes.js
+++ src/routes/productos.routes.js
@@ -8,6 +8,6 @@ const md_roles = require('../middlewares/roles');
 const api = express.Router();
 
 api.post('/registrarProducto/:idCategoria',[md_autenticacion.Auth,md_roles.verAdmin],controladorProducto.AgregarProducto);
-
-
+api.put('/editarProducto/:idCategoria/:idPructos',[md_autenticacion.Auth,md_roles.verAdmin],controladorProducto.EditarProducto)
+api.delete('/eliminarProducto/:idCategoria/:idPructos',[md_autenticacion.Auth,md_roles.verAdmin],controladorProducto.eliminarProducto)
 module.exports = api;
